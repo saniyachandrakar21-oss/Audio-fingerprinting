4ef42c0dedf722978c8c6928e203d4f7f8e5ca87
saniyachandrakar21-oss <saniyachandrakar21@gmail.com>
2026-06-27 05:43:06 +0530

    Initial project upload

---
+++ app1.py
@@ -0,0 +1,491 @@
+from fingerprint_engine import (
+    build_database,
+    generate_query_hashes,
+    song_identifier,
+    plot_offset_histogram,
+    get_library_data
+)
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import librosa
+import librosa.display
+
+import matplotlib.pyplot as plt
+import tempfile
+import os
+
+SONG_FOLDER = "songs"
+database = build_database(SONG_FOLDER)
+st.set_page_config(
+    page_title="EE200 Audio Fingerprinting",
+    layout="wide"
+)
+
+st.title("🎵 EE200: Audio Fingerprinting")
+
+st.caption(
+    "Index a library of songs as spectrogram fingerprints, then identify any short clip against it."
+)
+
+tab1, tab2, tab3 = st.tabs(
+    ["📚 Library", "🎯 Identify", "📦 Batch"]
+)
+with tab1:
+
+    st.subheader("Library")
+
+    library_data = get_library_data(
+        SONG_FOLDER
+    )
+
+    cols = st.columns(4)
+
+    for idx, song in enumerate(library_data):
+
+        with cols[idx % 4]:
+
+            st.container(border=True)
+            song_name = os.path.splitext(song["name"])[0]
+
+            st.markdown(
+                f"""
+                <div style="
+                    height:70px;
+                    overflow:hidden;
+                    text-align:center;
+                ">
+                    <b>{song_name}</b><br>
+                    {song['hashes']:,} hashes
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+
+            fig, ax = plt.subplots(
+                figsize=(3,2)
+            )
+
+            peaks = song["peaks"]
+
+            freq = song["frequency"]
+
+            time_vals = song["time"]
+
+            ax.set_facecolor("black")
+
+            colors = [
+                "#00FFFF",   # cyan
+                "#FFD700",   # gold
+                "#FF69B4",   # pink
+                "#AD8CFF",   # purple
+                "#7CFC00"    # green
+            ]
+            display_peaks = peaks[::3]
+            ax.scatter(
+                time_vals[peaks[:,1]],
+                freq[peaks[:,0]],
+                s=0.1,
+                c=colors[idx % len(colors)]
+            )
+            
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+            ax.set_xlabel("")
+            ax.set_ylabel("")
+
+            for spine in ax.spines.values():
+                spine.set_visible(False)
+
+            st.pyplot(fig)
+with tab2:
+
+    st.subheader("Identify a Clip")
+
+    uploaded_file = st.file_uploader(
+        "Upload Audio",
+        type=["wav","mp3"],
+        key="identify"
+    )
+
+    if uploaded_file:
+        st.audio(uploaded_file)
+        if st.button("Try"):
+            with tempfile.NamedTemporaryFile(
+                delete=False,
+                suffix=".mp3"
+            ) as tmp:
+
+                tmp.write(uploaded_file.getvalue())
+
+                query_path = tmp.name
+
+            audio, fs = librosa.load(
+                query_path,
+                sr=None
+            )
+            y = audio
+            sr = fs
+
+            query_hashes, constellation_data = generate_query_hashes(
+                query_path
+            )
+            matched_song, offset_count = song_identifier(
+                query_hashes[0]["hashes"],
+                database
+            )
+
+            if matched_song:
+
+                st.markdown(f"""
+                ## 🎯 MATCH FOUND
+
+                ### {matched_song}
+                """)
+
+                st.markdown(
+                     """
+                ## STEP 1 : SPECTROGRAM GENERATION
+
+                The uploaded audio clip is converted into a spectrogram, providing a time-frequency representation of the signal.
+                """)
+
+                fig, ax = plt.subplots(figsize=(4,3))
+
+                D = librosa.amplitude_to_db(
+                    np.abs(librosa.stft(audio)),
+                    ref=np.max
+                )
+
+                librosa.display.specshow(
+                    D,
+                    sr=fs,
+                    x_axis="time",
+                    y_axis="hz",
+                    ax=ax
+                )
+                ax.set_ylim(0, 5000)
+
+                left, center, right = st.columns([1,2,1])
+
+                with center:
+                    st.pyplot(fig)
+
+                st.markdown(
+                    """
+                ## STEP 2 : PEAK DETECTION & FINGERPRINT GENERATION
+
+                Prominent spectral peaks are extracted to form a constellation map, and peak pairs are converted into fingerprint hashes.
+                """
+                )
+                col1, col2 = st.columns(2)
+                with col1:
+
+                    fig1, ax1 = plt.subplots(figsize=(6,4))
+
+                    peaks = constellation_data[0]["peaks"]
+
+                    frequency = constellation_data[0]["frequency"]
+
+                    time_vals = constellation_data[0]["time"]
+
+                    spec_in_db = constellation_data[0]["spec_in_db"]
+
+                    ax1.pcolormesh(
+                        time_vals,
+                        frequency,
+                        spec_in_db,
+                        shading="gouraud"
+                    )
+
+                    ax1.scatter(
+                        time_vals[peaks[:,1]],
+                        frequency[peaks[:,0]],
+                        c="red",
+                        s=4
+                    )
+
+                    ax1.set_title(
+                        "Spectrogram with Peaks"
+                    )
+                    ax1.set_ylim(0, 5000)
+
+                    st.pyplot(fig1)
+
+                with col2:
+
+                    fig2, ax2 = plt.subplots(figsize=(6,4))
+
+                    ax2.scatter(
+                        time_vals[peaks[:,1]],
+                        frequency[peaks[:,0]],
+                        c="red",
+                        s=4
+                    )
+
+                    ax2.set_title(
+                        "Constellation Map"
+                    )
+
+                    st.pyplot(fig2)
+                st.divider()
+
+                st.metric(
+                    "Fingerprint Hashes Generated",
+                    len(query_hashes[0]["hashes"])
+                )
+
+                st.divider()
+                st.markdown(
+                """
+                # STEP 3 : DATABASE MATCHING
+
+                The generated fingerprint hashes are compared with the database, and an offset histogram is used to identify the song with the strongest alignment.
+                """)
+
+                hist_fig = plot_offset_histogram(
+                    matched_song,
+                    offset_count
+                )
+
+                st.pyplot(hist_fig)
+
+                st.success(
+                    f"Matched Song : {matched_song}"
+                )
+            
+        
+with tab3:
+
+    st.subheader("Identify Many Clips")
+
+    files = st.file_uploader(
+        "Upload Query Clips",
+        type=["wav","mp3"],
+        accept_multiple_files=True,
+        key="batch"
+    )
+
+    if files:
+
+        st.write(
+            f"{len(files)} file(s) selected"
+        )
+
+        if st.button("Run Batch"):
+
+            if len(files) > 5:
+
+                st.error("Maximum 5 files allowed")
+
+            else:
+                results = []
+                for uploaded_file in files:
+
+                    st.divider()
+
+                    st.header(uploaded_file.name)
+
+                    with tempfile.NamedTemporaryFile(
+                        delete=False,
+                        suffix=".mp3"
+                    ) as tmp:
+
+                        tmp.write(
+                            uploaded_file.getvalue()
+                        )
+
+                        query_path = tmp.name
+
+                    audio, fs = librosa.load(
+                        query_path,
+                        sr=None
+                    )
+
+                    query_hashes, constellation_data = generate_query_hashes(
+                        query_path
+                    )
+
+                    matched_song, offset_count = song_identifier(
+                        query_hashes[0]["hashes"],
+                        database
+                    )
+
+                    if matched_song:
+
+                        st.success(
+                            f"Matched Song : {matched_song}"
+                        )
+                        prediction = os.path.splitext(
+                            matched_song
+                        )[0]
+
+                        results.append({
+                            "filename": uploaded_file.name,
+                            "prediction": prediction
+                        })
+
+                        # =====================
+                        # STEP 1
+                        # =====================
+
+                        st.markdown("""
+                        # STEP 1 : SPECTROGRAM GENERATION
+
+                        The uploaded audio clip is converted into a spectrogram, providing a time-frequency representation of the signal. This serves as the foundation for extracting unique audio features.
+                        """)
+                        fig, ax = plt.subplots(
+                            figsize=(4,3)
+                        )
+
+                        D = librosa.amplitude_to_db(
+                            np.abs(librosa.stft(audio)),
+                            ref=np.max
+                        )
+
+                        librosa.display.specshow(
+                            D,
+                            sr=fs,
+                            x_axis="time",
+                            y_axis="hz",
+                            ax=ax
+                        )
+
+                        ax.set_ylim(0,5000)
+
+                        st.pyplot(fig)
+
+                        # =====================
+                        # STEP 2
+                        # =====================
+
+                        st.markdown("""
+                        # STEP 2 : PEAK DETECTION & FINGERPRINT GENERATION
+
+                        Prominent spectral peaks are extracted to form a constellation map, and peak pairs are converted into fingerprint hashes for efficient song identification.
+                        """)
+
+                        col1, col2 = st.columns(2)
+
+                        peaks = constellation_data[0]["peaks"]
+
+                        frequency = constellation_data[0]["frequency"]
+
+                        time_vals = constellation_data[0]["time"]
+
+                        spec_in_db = constellation_data[0]["spec_in_db"]
+
+                        with col1:
+
+                            fig1, ax1 = plt.subplots(
+                                figsize=(5,3)
+                            )
+
+                            ax1.pcolormesh(
+                                time_vals,
+                                frequency,
+                                spec_in_db,
+                                shading="gouraud"
+                            )
+
+                            ax1.scatter(
+                                time_vals[peaks[:,1]],
+                                frequency[peaks[:,0]],
+                                c="red",
+                                s=4
+                            )
+
+                            ax1.set_title(
+                                "Spectrogram with Peaks"
+                            )
+
+                            st.pyplot(fig1)
+
+                        with col2:
+
+                            fig2, ax2 = plt.subplots(
+                                figsize=(5,3)
+                            )
+                            fig2.patch.set_facecolor("white")
+                            ax2.set_facecolor("white")
+                            ax2.scatter(
+                                time_vals[peaks[:,1]],
+                                frequency[peaks[:,0]],
+                                c="red",
+                                s=4
+                            )
+
+                            ax2.set_xticks([])
+
+                            ax2.set_yticks([])
+
+                            ax2.set_title(
+                                "Constellation Map"
+                            )
+
+                            st.pyplot(fig2)
+
+                        st.divider()
+
+                        st.metric(
+                            "Fingerprint Hashes Generated",
+                            len(
+                                query_hashes[0]["hashes"]
+                            )
+                        )
+
+                        st.divider()
+
+                        # =====================
+                        # STEP 3
+                        # =====================
+
+                        st.markdown("""
+                        # STEP 3 : DATABASE MATCHING
+
+                        The generated fingerprint hashes are compared with the database, and an offset histogram is used to identify the song with the strongest alignment.
+                        """)
+
+                        hist_fig = plot_offset_histogram(
+                            matched_song,
+                            offset_count
+                        )
+
+                        st.pyplot(hist_fig)
+                        st.success(
+                            f"Final Match : {matched_song}"
+                        )
+
+                    else:
+
+                        st.error(
+                            f"No Match Found for {uploaded_file.name}"
+                        )
+
+                        results.append({
+                            "filename": uploaded_file.name,
+                            "prediction": "No Match"
+                        })
+                st.divider()
+
+                st.subheader("Batch Results")
+
+                results_df = pd.DataFrame(results)
+
+                st.dataframe(
+                    results_df,
+                    use_container_width=True
+                )
+
+                csv = results_df.to_csv(
+                    index=False
+                )
+
+                st.download_button(
+                    "Download results.csv",
+                    csv,
+                    file_name="results.csv",
+                    mime="text/csv"
+                )
---
+++ fingerprint_engine.py
@@ -0,0 +1,297 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from scipy.signal import spectrogram
+from skimage.feature import peak_local_max
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import tempfile
+import os
+
+def load_song(song_paths):
+    
+   songs = []
+   for path in song_paths :
+       
+        audio , f_s= librosa.load(path , sr= None)
+
+        songs. append({ "name": os.path.basename(path), "audio" : audio , "fs" :f_s })
+      
+   return songs
+
+def plot_spectrograms(songs,nperseg=4095,show_plot=True):
+    spectrograms = []
+
+    for song in songs:
+        name = song["name"]
+        audio = song["audio"]
+        fs = song["fs"]
+
+        frequency, time, spec_matrix = spectrogram(audio,fs,nperseg=nperseg)
+        spectrograms.append({"name": name,"frequency": frequency,"time": time,"spec_matrix": spec_matrix})
+
+        if show_plot:
+
+            plt.figure(figsize=(4,3))
+            plt.pcolormesh(time,frequency,10*np.log10(spec_matrix + 1e-10),shading='gouraud')
+            plt.xlim(10,40)
+            plt.ylim(0,5000)
+            plt.title(f"Spectrogram of {name}")
+            plt.xlabel("Time (in sec)")
+            plt.ylabel("Frequency (Hz)")
+            plt.colorbar(label="Power (dB)")
+
+            plt.tight_layout()
+            plt.show()
+
+    return spectrograms
+def plot_constellation_maps(spectrograms, min_distance = 11 , percentile_threshold = 95 , show_plot= True):
+    constellation_data = []
+    for spec in spectrograms:
+        name = spec["name"]
+        frequency = spec["frequency"]
+        time = spec["time"]
+        spec_matrix = spec["spec_matrix"]
+        
+        spec_in_db = 10*np.log10(spec_matrix + 1e-10)
+        peaks = peak_local_max( spec_in_db , min_distance=min_distance, threshold_abs=np.percentile(spec_in_db,percentile_threshold))
+
+        constellation_data.append({"name": name ,"peaks": peaks, "frequency": frequency , "time": time, "spec_in_db": spec_in_db } )
+        if show_plot:
+            
+            # plotting the maps 
+            fig, ax = plt.subplots(1, 2, figsize=(14,5))
+            # plotting peaks in spectrogram 
+        
+            ax[0].pcolormesh(time,frequency,spec_in_db,shading='gouraud')
+            ax[0].scatter( time[peaks[:,1]],frequency[peaks[:,0]],c='red',s=2)
+            ax[0].set_xlim(10,40)
+            ax[0].set_ylim(0,5000)
+            ax[0].set_title("Spectrogram with Peaks")
+            ax[0].set_xlabel("Time (in sec)")
+            ax[0].set_ylabel("Frequency (Hz)") 
+
+        # plottimg constellation map separately 
+       
+            ax[1].scatter(time[peaks[:,1]],frequency[peaks[:,0]],s=4)
+            ax[1].set_xlim(10,40)
+            ax[1].set_ylim(0,5000)
+            ax[1].set_title(f"Constellation Map : {name}")
+            ax[1].set_xlabel("Time (in sec)")
+            ax[1].set_ylabel("Frequency (Hz)")
+
+        
+            plt.tight_layout()
+            plt.show()
+    return constellation_data
+
+def hash_database(constellation_data , num_pairs= 5):
+    database = {}
+    for song in constellation_data:
+        name = song["name"]
+        peaks = song["peaks"]
+        peaks = peaks[np.argsort(peaks[:,1])]
+        for i in range(len(peaks)):
+            f1 = peaks[i][0]
+            t1 = peaks[i][1]
+            for j in range( i+1, min(i+num_pairs+1,len(peaks))):
+                f2 = peaks[j][0]
+                t2 = peaks[j][1]
+                dt = t2 - t1
+                if dt <= 0:
+                     continue
+                    
+                hash_key = (int(f1), int(f2), int(dt) )
+
+                if hash_key not in database:
+                    database[hash_key] = []
+                database[hash_key].append((name , int(t1)))
+    return database
+
+def build_database( Song_Folder):
+    #creating list of audios
+    song_paths=get_audio_paths(Song_Folder)
+   # Song loading  
+    songs = load_song(song_paths) 
+    # Step 1: Spectrograms
+    spectrograms = plot_spectrograms(songs, show_plot = False )
+
+    # Step 2: Constellation maps
+    constellation_data = plot_constellation_maps(spectrograms, show_plot = False )
+
+    # Step 3: Hash database
+    songs_database = hash_database(constellation_data)
+    return songs_database
+
+def get_audio_paths(path):
+    
+    audio_paths = []
+   # Single audio file
+    if os.path.isfile(path):
+        audio_paths.append(path)
+    #  Folder containing audio files
+    elif os.path.isdir(path):
+        
+        for file in os.listdir(path):
+            
+            if file.endswith(".mp3"):
+                full_path = os.path.join(path, file)
+                audio_paths.append(full_path)
+
+    else:
+        print("Invalid path")
+
+    return audio_paths
+
+def generate_query_hashes(Query_Folder, num_target_peaks=5):
+    
+    Query_paths= get_audio_paths(Query_Folder)
+    Queries =load_song(Query_paths) # songs as list of dict
+    spectrograms = plot_spectrograms(Queries, show_plot = False )
+    constellation_data = plot_constellation_maps(spectrograms, show_plot = False )
+    query_hashes = []
+    for query in constellation_data:
+        name = query["name"]
+        peaks = query["peaks"]
+        peaks = peaks[np.argsort(peaks[:,1])]
+        hashes = []
+        for i in range(len(peaks)):
+            
+            f1 = peaks[i][0]
+            t1 = peaks[i][1]
+            for j in range( i+1,min(i+num_target_peaks+1, len(peaks))):
+                f2 = peaks[j][0]
+                t2 = peaks[j][1]
+                dt = t2 - t1
+                if dt <= 0:
+                    continue
+                    
+
+                hash_key = ( int(f1), int(f2), int(dt))
+
+                hashes.append( (hash_key, int(t1))) # hash and time stamp
+                
+        query_hashes.append({"name": name,"hashes": hashes}) #complete hash 
+
+    return query_hashes, constellation_data
+
+def song_identifier(query_hashes, songs_database):
+
+    offset_count = {}
+    for hash_key, query_time in query_hashes:
+        if hash_key not in songs_database:
+            continue
+        matches = songs_database[hash_key]
+
+        for song_name, db_time in matches:
+            offset = db_time - query_time
+            match_key = ( song_name, offset )
+
+            if match_key not in offset_count:
+                offset_count[match_key] = 0
+            offset_count[match_key] += 1
+    if len(offset_count) == 0:
+        print("No match found")
+        return None , None
+    best_match = max(offset_count, key=offset_count.get)
+    best_count = offset_count[best_match]
+    if best_count < 10:  
+        print("Unable to identify song")
+        return None, offset_count
+    matched_song = best_match[0]
+    print("Matched Song:", matched_song)
+
+    print( "Match Count:",offset_count[best_match])
+
+    return matched_song , offset_count
+def plot_offset_histogram(matched_song, offset_count):
+
+    offsets = []
+    counts = []
+
+    for (song_name, offset), count in offset_count.items():
+
+        if song_name == matched_song:
+
+            offsets.append(offset)
+            counts.append(count)
+
+    if len(offsets) == 0:
+        return None
+
+    if len(offsets) == 0:
+        print("No offsets to plot")
+        return
+    fig, ax = plt.subplots(figsize=(12,6))
+
+    ax.bar(offsets, counts,width=30,color='blue')
+
+    ax.set_title(
+        f"Offset Histogram : {matched_song}"
+    )
+
+    ax.set_xlabel("Offset")
+
+    ax.set_ylabel("Match Count")
+
+    return fig
+def get_library_data(song_folder):
+
+    song_paths = get_audio_paths(song_folder)
+
+    songs = load_song(song_paths)
+
+    spectrograms = plot_spectrograms(
+        songs,
+        show_plot=False
+    )
+
+    constellation_data = plot_constellation_maps(
+        spectrograms,
+        show_plot=False
+    )
+
+    library_data = []
+
+    for song in constellation_data:
+
+        name = song["name"]
+
+        peaks = song["peaks"]
+
+        peaks = peaks[np.argsort(peaks[:,1])]
+
+        hash_count = 0
+
+        num_pairs = 5
+
+        for i in range(len(peaks)):
+
+            for j in range(
+                i + 1,
+                min(i + num_pairs + 1, len(peaks))
+            ):
+
+                dt = peaks[j][1] - peaks[i][1]
+
+                if dt > 0:
+
+                    hash_count += 1
+
+        library_data.append({
+
+            "name": name,
+
+            "hashes": hash_count,
+
+            "peaks": song["peaks"],
+
+            "frequency": song["frequency"],
+
+            "time": song["time"]
+
+        })
+
+    return library_data
